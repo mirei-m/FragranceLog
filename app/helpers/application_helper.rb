61ea5331b57ac60d1c48896167f65e3becbbbd6e
mirei <story.seller1213@gmail.com>
2025-06-12 21:04:46 +0900

    add:helperにページタイトルのメソッド定義しtitleタグに使用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "FragranceLog"
+    if page_title.present?
+      "#{page_title}｜#{base_title}"
+    else
+      base_title
+    end
+  end
 end
