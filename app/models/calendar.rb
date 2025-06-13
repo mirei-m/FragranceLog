16bfb0908c7236996f9fdba4369b0fe18bd1d398
mirei <story.seller1213@gmail.com>
2025-06-13 17:03:32 +0900

    add:カレンダーに記録できるように

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,4 +5,20 @@ class Calendar < ApplicationRecord
   validates :fragrance_id, presence: true
   validates :start_time, presence: true
   validates :user_id, uniqueness: { scope: :start_time, message: "この日はすでに記録があります" }
+
+  enum weather: {
+    sunny: 0,
+    cloudy: 1,
+    rainy: 2,
+    snowy: 3,
+  }, _prefix: :weather
+
+  enum mood: {
+    happy: 0,
+    relaxed: 1,
+    neutral: 2,
+    sad: 3,
+    angry: 4,
+    tired: 5,
+  }, _prefix: :mood
 end
