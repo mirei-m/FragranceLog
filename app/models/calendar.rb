e3def7341af86b11624c25246f3571371db9dfe7
mirei <story.seller1213@gmail.com>
2025-06-13 18:09:55 +0900

    add:カレンダー周りのi18n化

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,7 +2,6 @@ class Calendar < ApplicationRecord
   belongs_to :user
   belongs_to :fragrance
 
-  validates :fragrance_id, presence: true
   validates :start_time, presence: true
   validates :user_id, uniqueness: { scope: :start_time, message: "この日はすでに記録があります" }
 
