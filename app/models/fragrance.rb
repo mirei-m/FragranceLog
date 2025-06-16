96f775abcc7b7247534716fd698d87f65985de92
mirei <story.seller1213@gmail.com>
2025-06-16 17:56:57 +0900

    add:enum_help導入しマイ香水のステータスを表示

--- app/models/fragrance.rb
+++ app/models/fragrance.rb
@@ -2,7 +2,7 @@ class Fragrance < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :brand, presence: true, length: { maximum: 50 }
 
-  enum status: { private: 0, public: 1 }
+  enum status: { unpublished: 0, published: 1 }
 
   belongs_to :user
   has_one_attached :image
