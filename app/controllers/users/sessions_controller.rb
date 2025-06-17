3d0fa89a4e5383b531f90644195b139840335c70
mirei <story.seller1213@gmail.com>
2025-06-17 18:06:17 +0900

    add:診断ページのi18n化

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,10 +26,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    root_path
+    calendars_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    root_path
   end
 end
