0aa6082a2c36dcc1e62900c594d2c657afdef927
mirei <story.seller1213@gmail.com>
2025-06-19 11:05:37 +0900

    add:fragranceモデルに香りの印象カラムついか

--- app/models/fragrance.rb
+++ app/models/fragrance.rb
@@ -1,6 +1,9 @@
 class Fragrance < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :brand, presence: true, length: { maximum: 50 }
+  validates :sweetness, :freshness, :floral, :calm, :sexy, :spicy,
+          allow_nil: true,
+          numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 
   enum status: { unpublished: 0, published: 1 }
 
