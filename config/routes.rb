8e0859bcd048dc32f53dcb71e5657667b51bed72
mirei <story.seller1213@gmail.com>
2025-08-12 14:26:34 +0900

    add:profileのルーティングとコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resource :diagnosis, only: [ :new, :create ] do
     get :result
   end
+
+  resource :profile, only: %i[show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
