962af0239eddc61404e68b9539d9d52d4529c5e9
mirei <story.seller1213@gmail.com>
2025-08-14 13:52:26 +0900

    fix:プライバシポリシーと利用規約ページのログインチェックをスキップするよう設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :top ]
+  skip_before_action :authenticate_user!, only: [ :top, :terms_of_service, :privacy_policy ]
 
   def top
   end
