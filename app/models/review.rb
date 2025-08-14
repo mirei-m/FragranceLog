76ff378ce3374ec45e85c7fb3742fd8c45b3ecae
mirei <story.seller1213@gmail.com>
2025-08-14 14:10:48 +0900

    add:commentのバリデーションとアソシエーション記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :fragrance
 
+  has_many :comments, dependent: :destroy
+
   validates :body, presence: true, length: { maximum: 1000 }
   validates :fragrance_id, uniqueness: { scope: :user_id, message: "はすでにレビュー済みです" }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :fragrances, dependent: :destroy
   has_many :calendars, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_one_attached :profile_image
 
