ec74652840014094e6b59eb54f7cb13e23898932
mirei <story.seller1213@gmail.com>
2025-08-22 11:21:29 +0900

    add:favorite関連のバリデーションとアソシエーション記述

--- db/migrate/20250822021610_create_favorites.rb
+++ db/migrate/20250822021610_create_favorites.rb
@@ -1,7 +1,13 @@
 class CreateFavorites < ActiveRecord::Migration[7.2]
   def change
     create_table :favorites do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :review, null: false, foreign_key: true
+
       t.timestamps
     end
+
+    # 同じユーザーが同じレビューを複数回お気に入りできないように
+    add_index :favorites, [ :user_id, :review_id ], unique: true
   end
 end
