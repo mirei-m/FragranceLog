deac5b285e34f276b23514a3efa20812f294ffba
mirei <story.seller1213@gmail.com>
2025-08-22 11:45:21 +0900

    add:お気に入り・解除用のアクション記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,24 @@
 class FavoritesController < ApplicationController
-  
+  def create
+    @review = Review.find(params[:review_id])
+    @favorite = current_user.favorites.build(review: @review)
+
+    if @favorite.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to review_path(@review) }
+      end
+    end
+  end
+
+  def destroy
+    @favorite = current_user.favorites.find(params[:id])
+    @review = @favorite.review
+    @favorite.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to review_path(@review) }
+    end
+  end
 end
