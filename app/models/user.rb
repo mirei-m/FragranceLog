2564a27a6dbd89ea70a80867f4196b7c000040c2
mirei <story.seller1213@gmail.com>
2025-09-12 19:09:07 +0900

    add:user.rbにバリデーションやomniauth用の設定を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 
   has_many :fragrances, dependent: :destroy
   has_many :calendars, dependent: :destroy
@@ -14,11 +15,23 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, presence: true, length: { maximum: 255 }
+  validates :uid, uniqueness: { scope: :provider }, allow_nil: true
   validates :profile_image, content_type: { in: %w[image/jpeg image/gif image/png] },
                           size: { less_than: 5.megabytes },
                           if: -> { profile_image.attached? }
 
   def own?(object)
     id == object.user_id
+  end
+
+  # Google認証用
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0, 20]
+      user.provider = auth.provider
+      user.uid = auth.uid
+    end
   end
 end
