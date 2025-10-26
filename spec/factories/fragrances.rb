19e91ff6fc7d2d3f8819fc9deb7fa075be162a6f
mirei <story.seller1213@gmail.com>
2025-10-26 14:25:06 +0900

    add:user,fragrance,reviewのfactory記述

--- spec/factories/fragrances.rb
+++ spec/factories/fragrances.rb
@@ -1,4 +1,19 @@
 FactoryBot.define do
   factory :fragrance do
+    sequence(:name) { |n| "香水#{n}" }
+    sequence(:brand) { |n| "ブランド#{n}" }
+    status { :unpublished }
+    sweetness { rand(1..5) }
+    freshness { rand(1..5) }
+    floral { rand(1..5) }
+    calm { rand(1..5) }
+    sexy { rand(1..5) }
+    spicy { rand(1..5) }
+
+    association :user
+
+    trait :published do
+      status { :published }
+    end
   end
 end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :review do
-    
+    body { "body" }
+    association :user
+    association :fragrance
+
+    trait :max_length do
+      body { "a" * 1000 }
+    end
+
+    trait :too_long do
+      body { "a" * 1001 }
+    end
+
+    trait :empty_body do
+      body { "" }
+    end
   end
 end
